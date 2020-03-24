a847661be12673e0256275475e1d809313f37b29
Atiwat Deepo <atiwat.deepo@gmail.com>
2020-03-24 23:48:53 +0700

    add Pawn constructor, getSymbol()

--- src/entity/Pawn.java
+++ src/entity/Pawn.java
@@ -1,5 +1,30 @@
 package entity;
 
-public class Pawn {
+import java.awt.Point;
+
+import entity.base.Entity;
+import logic.Side;
+import logic.Sprites;
+
+public class Pawn extends Entity {
+
+	public Pawn(Point p, Side side) {
+		super(p, side);
+	}
+
+	@Override
+	public int getSymbol() {
+		// TODO Auto-generated method stub
+		if (this.side == Side.Black) {
+			return Sprites.B_PAWN;
+		}
+		return Sprites.B_PAWN;
+	}
+
+	@Override
+	public Point[] canMove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
