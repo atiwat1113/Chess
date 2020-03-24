6eaa9e3f34bd587696133dfe37fed812cedd7271
Atiwat Deepo <atiwat.deepo@gmail.com>
2020-03-24 23:55:46 +0700

    add Bishop constructor, getSymbol()

--- src/entity/Bishop.java
+++ src/entity/Bishop.java
@@ -1,5 +1,30 @@
 package entity;
 
-public class Bishop {
+import java.awt.Point;
+
+import entity.base.Entity;
+import logic.Side;
+import logic.Sprites;
+
+public class Bishop extends Entity{
+
+		public Bishop(Point p,Side side) {
+			super(p,side);
+		}
+	
+	@Override
+	public int getSymbol() {
+		// TODO Auto-generated method stub
+		if (this.side == Side.Black) {
+			return Sprites.B_BISHOP;
+		}
+		return Sprites.W_BISHOP;
+	}
+
+	@Override
+	public Point[] canMove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
