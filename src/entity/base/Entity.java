022994975c2cdf116cfe61bb7222777ad5dfebe9
Atiwat Deepo <atiwat.deepo@gmail.com>
2020-03-24 19:01:01 +0700

    change move() and canEat() method. check the comment

--- src/entity/base/Entity.java
+++ src/entity/base/Entity.java
@@ -1,29 +1,49 @@
 package entity.base;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 import logic.Board;
 import logic.Side;
-
 
 public abstract class Entity { // แก้ private --> protected
 	protected Point p;
 	protected boolean isMove;
 	protected Side side;
-	
-	public Entity(Point p,Side side) {// Point (y,x)
+
+	public Entity(Point p, Side side) {// Point (y,x)
 		this.p = p;
 		this.side = side; // น่าจะต้องบอกฝั่งไว้ตั้งแต่ตอนสร้างหมาก
 		isMove = false;
 	}
-	
+
 	public abstract int getSymbol();
-	public abstract boolean move(Point p);
+
 	public abstract Point[] canMove();
-	public abstract Point[] canEat(Board board);
-	
-	public void remove() {//----------------------------------------
-		
+
+	public boolean move(Point p) { // การ move ของทุกหมากมีรูปแบบเดียวกัน
+		for (Point moveablePoint : this.canMove()) {
+			if (moveablePoint.equals(p)) {
+				this.p = p;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public Point[] canEat(Board board) { // การเช็คช่องที่กินได้ของทุกหมากมีวิธีการเช็คเหมือนกัน
+		ArrayList<Point> eatablePoint = new ArrayList<Point>();
+		for (Point moveablePoint : this.canMove()) {
+			if (board.getEntity(moveablePoint.x, moveablePoint.y) != null
+					&& board.getEntity(moveablePoint.x, moveablePoint.y).getSide() != this.side) {
+				eatablePoint.add(moveablePoint);
+			}
+		}
+		return (Point[]) eatablePoint.toArray();
+	}
+
+	public void remove() {// ----------------------------------------
+
 	}
 
 	public Point getP() {
