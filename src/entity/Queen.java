d6ff7d725f6e8d18826fceb4eb600986050ad389
Atiwat Deepo <atiwat.deepo@gmail.com>
2020-03-24 23:51:30 +0700

    add Queen constructor, getSymbol()

--- src/entity/Queen.java
+++ src/entity/Queen.java
@@ -1,5 +1,30 @@
 package entity;
 
-public class Queen {
+import java.awt.Point;
+
+import entity.base.Entity;
+import logic.Side;
+import logic.Sprites;
+
+public class Queen extends Entity{
+
+		public Queen (Point p,Side side) {
+			super(p,side);
+		}
+	
+	@Override
+	public int getSymbol() {
+		// TODO Auto-generated method stub
+		if (this.side == Side.Black) {
+			return Sprites.B_QUEEN;
+		}
+		return Sprites.W_QUEEN;
+	}
+
+	@Override
+	public Point[] canMove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
