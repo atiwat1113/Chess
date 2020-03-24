6a63a273e5a08350c02542016e5707eb7b75921c
Atiwat Deepo <atiwat.deepo@gmail.com>
2020-03-24 17:20:49 +0700

    fix error in King's constructor

--- src/entity/King.java
+++ src/entity/King.java
@@ -3,15 +3,20 @@ package entity;
 import java.awt.Point;
 
 import entity.base.Entity;
+import logic.Side;
+import logic.Sprites;
 
 public class King extends Entity{
 	private boolean neverMove;//for castling
-	public King(Point p) {
-		super(p);
+	public King(Point p,Side side) {
+		super(p,side);
 		neverMove = true;
 	}
 	public int getSymbol() {
-		return 0;
+		if (this.side == Side.Black) {
+			return Sprites.B_KING;
+		}
+		return Sprites.W_KING;
 	}
 	public boolean move(Point p) {//--------------------
 		return true;
