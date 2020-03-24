12e5a6ce190e4abc11c3c76399486aa2e1b39f0f
Atiwat Deepo <atiwat.deepo@gmail.com>
2020-03-24 23:54:09 +0700

    add Rook constructor, getSymbol()

--- src/entity/Rook.java
+++ src/entity/Rook.java
@@ -1,5 +1,33 @@
 package entity;
 
-public class Rook {
+import java.awt.Point;
+
+import entity.base.Entity;
+import logic.Side;
+import logic.Sprites;
+
+public class Rook extends Entity {
+
+	private boolean neverMove;
+
+	public Rook(Point p, Side side) {
+		super(p, side);
+		this.neverMove = true;
+	}
+
+	@Override
+	public int getSymbol() {
+		// TODO Auto-generated method stub
+		if (this.side == Side.Black) {
+			return Sprites.B_ROOK;
+		}
+		return Sprites.W_ROOK;
+	}
+
+	@Override
+	public Point[] canMove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
